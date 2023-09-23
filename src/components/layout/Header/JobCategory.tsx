86bc22f1fc7a906f30f47194477327e6ddf6095c
Dibyalochan <dibyalochanparida@gmail.com>
2023-09-23 11:23:35 +0530

    added popover for job

---
+++ src/components/layout/Header/JobCategory.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const JobCategory = () => {
+    return (
+        <div className="p-5 bg-white w-screen max-w-sm">
+            <ul>
+                <li className="font-bold">
+                    Jobs in demand
+                </li>
+                <li>
+                    Fresher jobs
+                </li>
+                <li >
+                    MNC jobs
+                </li>
+                <li>
+                    Remote jobs
+                </li>
+                <li>
+                    Work from home jobs
+                </li>
+                <li >
+                    Walk-in jobs
+                </li>
+                <li >
+                    Part-time jobs
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default JobCategory;
