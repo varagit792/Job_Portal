2f704ed783ca0495a16f8606fdee36d3b0991563
Dibyalochan <dibyalochanparida@gmail.com>
2023-10-12 11:31:19 +0530

    added scroll top functionality in util

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import AllRoutes from './AllRoutes';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import { useLocation } from 'react-router';
+import { scrollToTop } from './components/utils/utils';
 
 const App = () => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-  });
+    scrollToTop();
   }, [location])
-  
+
   return (
     <div className="h-screen">
       {location?.pathname !== "/registration" && <Header />}
