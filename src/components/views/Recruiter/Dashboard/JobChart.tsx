1613b505ea7eec5bd402555c839bc1916e52f2ee
Dibyalochan <dibyalochanparida@gmail.com>
2023-10-30 16:33:11 +0530

    modified recharts for employer dashbord

--- src/components/views/Recruiter/Dashboard/JobChart.tsx
+++ src/components/views/Recruiter/Dashboard/JobChart.tsx
@@ -11,27 +11,22 @@ import {
 const data = [
     {
         name: "Product Designer",
-        uv: 320,
         pv: 180,
     },
     {
         name: "Sales Manager",
-        uv: 320,
         pv: 150,
     },
     {
         name: "Java Developer",
-        uv: 320,
         pv: 170,
     },
     {
         name: "HR Administration",
-        uv: 320,
         pv: 140,
     },
     {
         name: "Graphic Designer",
-        uv: 320,
         pv: 60,
     },
     {
@@ -43,7 +38,14 @@ const data = [
 
 const JobChart = () => {
     return (
-        <div className="mb-16 py-10 bg-[#EEF2FF] rounded-xl flex justify-center items-center">
+        <div className="mb-16 p-10 bg-[#EEF2FF] rounded-xl flex justify-center items-center flex-col">
+            <div className="mb-8 flex justify-between items-center w-full">
+                <h1 className="text-xl font-bold">Applications received for latest jobs posted</h1>
+                <div className="flex justify-end items-center">
+                    <div className=" flex justify-center items-center"><span className="bg-[#E0E7FF] w-2 h-2 block rounded-full mr-1"></span><span className="text-xs text-[#475569] mr-2">Total Applications</span></div>
+                    <div className=" flex justify-center items-center"><span className="bg-[#9546E5] w-2 h-2 block rounded-full mr-1"></span><span className="text-xs text-[#475569]">Applications Reviewed</span></div>
+                </div>
+            </div>
             <BarChart
                 width={800}
                 height={300}
