67772fe537e52754db4e903e04565651cf186e1e
varaprasad <varaprasad.urumadla@ratnaglobaltech.com>
2023-11-09 11:45:56 +0530

    company filter chips added

--- src/components/views/Companies/AllCompanies.tsx
+++ src/components/views/Companies/AllCompanies.tsx
@@ -13,7 +13,7 @@ import DominousIcon from '../../../assets/svg/DominousIcon.svg';
 import GoproIcon from '../../../assets/svg/GoproIcon.svg';
 import Rectangle_19 from '../../../assets/svg/Rectangle-19.svg';
 import { BiSearch } from 'react-icons/bi';
-import { getAllCompanies, clearGetAllCompaniesSlice, setDepartment, setFilterDepartment, setFilterLocation, setLocation, setCompanyType, setFilterCompanyType, setIndustry, setFilterIndustry, setCompany, setFilterCompany } from '../../../store/reducers/companies/getAllCompanies';
+import { getAllCompanies, clearGetAllCompaniesSlice, setDepartment, setFilterDepartment, setFilterLocation, setLocation, setCompanyType, setFilterCompanyType, setIndustry, setFilterIndustry, setCompany, setFilterCompany, clearIndividual, setNavigateFilterOption } from '../../../store/reducers/companies/getAllCompanies';
 import { CompanyDepartmentFilter } from './FilterByCompanyDepartment';
 import { scrollToTop } from '../../utils/utils';
 import FiltersModal from '../Companies/FiltersModal';
@@ -21,6 +21,7 @@ import { CompanyTypeFilter } from './FilterByCompanyType';
 import { CompanyLocationFilter } from './FilterByCompanyLocation';
 import { IndustryFilter } from './FilterByIndustry';
 import { CompanyFilter } from './FilterByCompany';
+import { RxCross2 } from 'react-icons/rx';
 
 const AllCompanies = () => {
     const dispatch = useAppDispatch();
@@ -31,7 +32,7 @@ const AllCompanies = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [companyCard, setCompanyCard] = useState<any>([]);
     
-    console.log("allFilterIds-->", allFilterIds);
+    console.log("allFilterIds-->", allFilterIds, filtersData);
     
     useEffect(() => {
         window.addEventListener("scroll", handelInfiniteScroll);
@@ -82,6 +83,11 @@ const AllCompanies = () => {
 
     const onClickCompanyCard = (companyId: any) => {
 		window.open(`/allCompanies/companyDescription/${companyId}`, '_blank');
+    }
+
+    const handleViewAll = () => {
+        setIsOpen(true);
+        dispatch(setNavigateFilterOption("CompanyType"));
     }
 
     const handleDepartmentCheckbox = (data: any) => { 
@@ -169,7 +175,106 @@ return (
             <div className="grid grid-cols-12 gap-10 px-32 bg-[#F8FAFC] py-6">
                 <div className="col-start-1 col-end-4">
                     <div className="bg-[#FFF] rounded-xl p-4 sticky top-[13%]">
-                        <h1 className="flex justify-between items-center leading-none"><span className="text-[#475569] font-bold">Filters</span><span className="bg-[#F8FAFC] rounded px-2 py-1 text-[#4F46E5]">4</span></h1>
+                    <h1 className="flex justify-between items-center leading-none"><span className="text-[#475569] font-bold">Filters</span><span className="bg-[#F8FAFC] rounded px-2 py-1 text-[#4F46E5]">4</span></h1>
+                    
+                    <div className="mt-5 flex flex-wrap justify-start items-center gap-2">
+                            {/* {filtersData?.companyType !== null && companyType?.map((item: any) => item?.id == filtersData?.companyType &&
+                                <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                    <span className="mr-1">0 - {item?.title}</span>
+                                    <span className="cursor-pointer"
+                                        onClick={() => {
+                                            dispatch(clearIndividual({ companyType: item?.id }))
+                                            setToggleDispach(true)
+                                        }}>
+                                        <RxCross2 />
+                                    </span>
+                                </span>
+                            )} */}
+                            {filtersData?.department?.map((item: any) => {
+                                const departmentFilter = department?.filter((departmentItem: any) => departmentItem?.id === item);
+                                return (
+                                    <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                        <span className="mr-1">{departmentFilter[0]?.title}</span>
+                                        <span className="cursor-pointer"
+                                            onClick={() => {
+                                                dispatch(clearIndividual({ department: departmentFilter[0]?.id }))
+                                                setToggleDispach(true)
+                                            }}>
+                                            <RxCross2 />
+                                        </span>
+                                    </span>
+                                )
+                            })}
+                            {filtersData?.location?.map((item: any) => {
+                                const locationFilter = location?.filter((locationItem: any) => locationItem?.id === item);
+                                return (
+                                    <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                        <span className="mr-1">{locationFilter[0]?.title}</span>
+                                        <span className="cursor-pointer"
+                                            onClick={() => {
+                                                dispatch(clearIndividual({ location: locationFilter[0]?.id }))
+                                                setToggleDispach(true)
+                                            }}>
+                                            <RxCross2 />
+                                        </span>
+                                    </span>
+                                )
+                            })}
+                            {filtersData?.company?.map((item: any) => {
+                                const companyFilter = company?.filter((companyItem: any) => companyItem?.id === item);
+                                return (
+                                    <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                        <span className="mr-1">{companyFilter[0]?.title}</span>
+                                        <span className="cursor-pointer"
+                                            onClick={() => {
+                                                dispatch(clearIndividual({ company: companyFilter[0]?.id }))
+                                                setToggleDispach(true)
+                                            }}>
+                                            <RxCross2 />
+                                        </span>
+                                    </span>
+                                )
+                            })}
+                            {filtersData?.companyType?.map((item: any) => {
+                                const companyTypeFilter = companyType?.filter((companyTypeItem: any) => companyTypeItem?.id === item);
+                                return (
+                                    <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                        <span className="mr-1">{companyTypeFilter[0]?.title}</span>
+                                        <span className="cursor-pointer"
+                                            onClick={() => {
+                                                dispatch(clearIndividual({ companyType: companyTypeFilter[0]?.id }))
+                                                setToggleDispach(true)
+                                            }}>
+                                            <RxCross2 />
+                                        </span>
+                                    </span>
+                                )
+                            })}
+                        {filtersData?.industry?.map((item: any) => {
+                                const industryFIlter = industry?.filter((industryItem: any) => industryItem?.id === item);
+                                return (
+                                    <span className="bg-[#F1F5F9] px-3 py-1.5 rounded-lg flex justify-start items-center text-xs">
+                                        <span className="mr-1">{industryFIlter[0]?.title}</span>
+                                        <span className="cursor-pointer"
+                                            onClick={() => {
+                                                dispatch(clearIndividual({ industry: industryFIlter[0]?.id }))
+                                                setToggleDispach(true)
+                                            }}>
+                                            <RxCross2 />
+                                        </span>
+                                    </span>
+                                )
+                            })}
+                            {((filtersData?.department !== undefined && filtersData?.department?.length !== 0)
+                                || (filtersData?.location !== undefined && filtersData?.location?.length !== 0)
+                                || (filtersData?.company !== undefined && filtersData?.company?.length !== 0)
+                                || (filtersData?.companyType !== undefined && filtersData?.companyType?.length !== 0)
+                                || (filtersData?.industry !== undefined && filtersData?.industry?.length !== 0)
+                            ) &&
+                                <button className=" border-b border-[#475569] text-[#475569] text-xs" onClick={handleViewAll}>All</button>
+                            }
+                        </div>
+
                         <hr className="bg-[#E0E7FF] my-5" />                        
                         <CompanyTypeFilter
                             handleCompanyTypeCheckbox={handleCompanyTypeCheckbox}
